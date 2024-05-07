a89cea9524b43b078f842b47689bea775c180b8e
arissketch <60331491+arissketch@users.noreply.github.com>
2024-05-07 23:21:37 +0000

    midway2

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,8 @@
 class PhotosController <ApplicationController
   def showphoto
+    the_id =params.fetch("id")
+    matching_photo=Photo.where({:id=> the_id})
+    @the_photo=matching_photo.at(0)
     render({:template => "photos/show"})
   end
 end
